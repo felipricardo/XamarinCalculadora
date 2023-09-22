ddb9f63f50b46082466b11e4174b8a400870c3a6
felipricardo <felipebarbosaok@gmail.com>
2023-09-22 15:22:23 -0300

    up

--- XamarinCalculadora/MainPage.xaml.cs
+++ XamarinCalculadora/MainPage.xaml.cs
@@ -10,9 +10,50 @@ namespace XamarinCalculadora
 {
     public partial class MainPage : ContentPage
     {
+        int currentState = 1;
+        string mathOperator;
+        double firstNumber, secondNumber;
         public MainPage()
         {
             InitializeComponent();
+            OnClear(new object(), new EventArgs());
+        }
+
+        void OnClear(object sender, EventArgs e)
+        {
+            firstNumber = 0;
+            secondNumber = 0;
+            currentState = 1;
+            this.resultText.Text = "0";
+        }
+
+        void OnSelectNumber(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string pressed = button.Text;
+
+            if(this.resultText.Text == "0" || currentState < 0)
+            {
+                this.resultText.Text = "";
+                if (currentState < 0)
+                    currentState *= -1;
+            }
+
+            this.resultText.Text += pressed;
+
+            double number;
+            if (double.TryParse(this.resultText.Text, out number))
+            {
+                this.resultText.Text = number.ToString("N0");
+                if (currentState == 1)
+                {
+                    firstNumber = number;
+                }
+                else
+                {
+                    secondNumber = number;
+                }
+            }
         }
     }
 }
